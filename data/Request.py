c8c9a19de8611e6a572995c41a5bf7654eeaad27
Ghiorghies Andrei-Cristian <andrey5015n@gmail.com>
2026-07-08 11:55:28 +0300

    Protocol

--- data/Request.py
+++ data/Request.py
@@ -1,3 +1,19 @@
+from enum import Enum
+
+class Category(Enum):
+    FACTUAL_KNOWLEDGE = 1
+    MATH = 2
+    SENTIMENT = 3
+    SUMMARISATION = 4
+    NER = 5
+    CODE_DEBUG = 6
+    LOGICAL = 7
+    CODE_GENERATION = 8
+    NONE = 9
+
 class Request:
-    def __init__(self, prompt):
+    def __init__(self, id, prompt):
+        self.id = id;
         self.prompt = prompt
+        self.category = Category.NONE
+        self.difficulty = 0.0 # min: 0.0; max 10.0
